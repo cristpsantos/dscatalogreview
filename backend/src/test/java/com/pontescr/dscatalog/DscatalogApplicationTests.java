d389150d3c401eac05648acee4f9f470cf019293
Cristiano Pontes <pontescr@gmail.com>
2023-06-21 21:15:16 -0300

    First commit

---
+++ backend/src/test/java/com/pontescr/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pontescr.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
